0604ab84ddcfadd4ad9e0af035ea73da6441540c
evanazh <evanazhr@gmail.com>
2026-01-26 12:01:26 +0700

    fix : complete footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,33 +1,41 @@
 import { Button } from "@/components/ui/button";
-import Orb from "@/components/ui/orb";
 import Link from "next/link";
 import Image from "next/image";
 
 export default function Footer() {
   return (
-    <footer className="w-full">
-      <div className="max-w-[1100px] relative mx-auto px-4 md:px-8">
-        <Orb
-          className="lg:-right-20  right-0 top-0 animate-pulse"
-          variant="orb-2"
-          size="md"
+    <footer className="w-full pt-40">
+      <div className="max-w-[1100px] relative mx-auto px-4 md:px-16">
+        <Image
+          src="/assets/backgrounds/ellipse/ellipse-red.svg"
+          className="absolute right-0 xl:-right-10 top-0"
+          width={45}
+          height={45}
+          alt=""
         />
         <div className="flex flex-col md:flex-row gap-8 relative items-center justify-between py-6 border-t-1 border-b-1 border-white/10">
-          <p className="text-[30px] text-center md:text-left font-bold">
+          <p className="text-[24px] md:text-[24px] text-center md:text-left font-bold">
             We&apos;ve prepared everything, it&apos;s time for you to tell the
             problem
           </p>
           <div className="flex gap-2">
-            <Button>Send Quote</Button>
-            <Button variant="highlight">Ask Us</Button>
+            <Link href={"/quote"}>
+              <Button>Send Quote</Button>
+            </Link>
+            <Link href={"/contact"}>
+              <Button variant="highlight">Ask Us</Button>
+            </Link>
           </div>
         </div>
-        <div className="flex flex-col relative md:flex-row md:justify-between md:items-start justify-center items-start gap-8 leading-9 py-8">
-          <Orb
-            className="lg:-left-30 animate-pulse left-0 top-0"
-            variant="orb-1"
-            size="default"
+        <div className="flex flex-col relative sm:flex-row md:justify-between md:items-start justify-center items-start gap-8 leading-9 py-24">
+          <Image
+            className="absolute left-0 md:-left-10 lg:-left-20 top-5"
+            src="/assets/backgrounds/ellipse/ellipse-green.svg"
+            width={55}
+            height={55}
+            alt=""
           />
+
           <div className="flex flex-col">
             <Link href={"/"} className="flex gap-2 font-bold text-lg mb-4">
               <Image
@@ -45,14 +53,14 @@ export default function Footer() {
             <p className="font-semibold uppercase text-base">Services</p>
             <ul>
               {[
-                { name: "Web Development", url: "/" },
-                { name: "App Development", url: "/" },
-                { name: "UI Design", url: "/" },
-                { name: "Consultation", url: "/" },
-                { name: "Maintenance", url: "/" },
+                { name: "Web Development", url: "/services#development" },
+                { name: "App Development", url: "/services#development" },
+                { name: "UI Design", url: "/services#ui-design" },
+                { name: "Consultation", url: "/services/details#faq" },
+                { name: "Maintenance", url: "/services#maintenance" },
               ].map((item, index) => {
                 return (
-                 <li key={index}>
+                  <li key={index}>
                     <Link
                       href={item.url}
                       className="text-white/60 hover:text-white"
@@ -68,12 +76,12 @@ export default function Footer() {
             <p className="font-semibold uppercase text-base">Company</p>
             <ul>
               {[
-                { name: "About", url: "/" },
-                { name: "Contact", url: "/" },
-                { name: "Send Quote", url: "/" },
+                { name: "About", url: "/about" },
+                { name: "Contact", url: "/contact" },
+                { name: "Send Quote", url: "/quote" },
                 { name: "Privacy Policy", url: "/" },
-                { name: "Term of Service", url: "/" },
-                { name: "Jobs", url: "/" },
+                { name: "Term of Service", url: "/term-of-service" },
+                { name: "Jobs", url: "/how-we-work" },
               ].map((item, index) => {
                 return (
                   <li key={index}>
