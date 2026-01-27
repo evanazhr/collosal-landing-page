373556521580686737d5569e72ed5d64f5f50d1a
evanazh <evanazhr@gmail.com>
2026-01-27 15:31:19 +0700

    style: change navbar breakpoint from md to lg

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -44,45 +44,45 @@ export default function Navbar() {
   return (
     <div
       ref={navRef}
-      className={`${scrollY > 80 ? " top-2 sticky mx-auto" : ""} max-w-[1100px] z-50 mx-auto duration-500 w-full px-4 md:px-16`}
+      className={`${scrollY > 80 ? "sticky top-2 mx-auto" : ""} z-50 mx-auto w-full max-w-[1100px] px-4 duration-500 md:px-16`}
     >
       <div
-        className={`${scrollY > 80 ? "bg-light backdrop-blur-md px-4 rounded-[5px]" : ""} ${isOpen ? "space-y-6" : "space-y-0"} py-6 flex md:flex-row flex-col justify-between items-start md:items-center md:justify-between `}
+        className={`${scrollY > 80 ? "bg-light rounded-[5px] px-4 backdrop-blur-md" : ""} ${isOpen ? "space-y-6" : "space-y-0"} flex flex-col items-start justify-between py-6 lg:flex-row lg:items-center lg:justify-between`}
       >
-        <div className="flex justify-between w-full md:w-fit">
-          <Link href={"/"} className="flex gap-2 justify-center items-center">
+        <div className="flex w-full justify-between lg:w-fit">
+          <Link href={"/"} className="flex items-center justify-center gap-2">
             <Image
               width={30}
               height={30}
               alt="collosal-logo"
               src={"/assets/icons/logo.svg"}
             />
-            <span className="font-bold text-2xl">Collosal</span>
+            <span className="text-2xl font-bold">Collosal</span>
           </Link>
           <Button
             variant="ghost"
             onClick={() => setIsOpen(!isOpen)}
-            className="flex items-center justify-center md:hidden"
+            className="flex items-center justify-center lg:hidden"
           >
             <Menu className="size-xl" />
           </Button>
         </div>
         <nav
-          className={`${isOpen ? "flex" : "hidden"} flex-col top-20 gap-4 items-start md:flex-row md:flex md:items-center justify-center md:gap-12`}
+          className={`${isOpen ? "flex" : "hidden"} top-20 flex-col items-start justify-center gap-4 md:gap-12 lg:flex lg:flex-row lg:items-center`}
         >
           {NavItems.map((item, index) => (
             <Link
               key={index}
               href={item.url}
               onClick={() => setIsOpen(false)} // 4. Tambahan: Tutup saat menu diklik
-              className="relative py-2 text-lg group hover:text-green"
+              className="group hover:text-green relative py-2 text-lg"
             >
               {item.name}
-              <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-green transition-all duration-300 group-hover:w-full"></span>
+              <span className="bg-green absolute bottom-0 left-0 h-0.5 w-0 transition-all duration-300 group-hover:w-full"></span>
             </Link>
           ))}
         </nav>
-        <div className={`${isOpen ? "flex" : "hidden"} md:flex`}>
+        <div className={`${isOpen ? "flex" : "hidden"} lg:flex`}>
           <Link href={"/contact"}>
             <Button variant={"highlight"}>Contact</Button>
           </Link>
